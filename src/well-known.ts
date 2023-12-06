fd1f10239249be8eba2e4314ce974dcfb60d37a1
Eddie Webbinaro <ollitech@gmail.com>
2023-12-06 13:57:50 -0500

    add proto

--- src/well-known.ts
+++ src/well-known.ts
@@ -1,8 +1,28 @@
 import express from 'express'
+import { resolve } from 'path'
 import { AppContext } from './config'
 
 const makeRouter = (ctx: AppContext) => {
   const router = express.Router()
+
+  const handles = new Map([
+    ["eddie","did:plc:r2jsoijmenfb67klwdc3hyav"]
+  ])
+
+
+  router.get('/.well-known/atproto-did', (req, res) => {
+    const handle = req.header['X-atproto-handle'];
+    console.log(handle);
+    if (typeof handle === "undefined") {
+      res.status(404).send()
+      return
+    }
+
+    const did = handles.get(handle[0]);
+    res.type('text/plain').send('did');
+  })
+
+
 
   router.get('/.well-known/did.json', (_req, res) => {
     if (!ctx.cfg.serviceDid.endsWith(ctx.cfg.hostname)) {
